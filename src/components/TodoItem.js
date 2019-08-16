66f252973f8b1174e8dbe7a5ecbe73b44c38820a
hiepn4759 <hiepn4759@gmail.com>
2019-08-16 08:36:39 +0700

    Todo_list

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class TodoIteam extends Component{
+    render() {
+        return(
+            <div className="TodoItem">
+                <h2>Nguyen Hiep</h2>
+            </div>
+        );
+    }
+}
+
+export default TodoIteam;
